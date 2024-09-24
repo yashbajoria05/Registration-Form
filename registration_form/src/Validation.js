9c00e4ee533805da055feaf2a96e9872bd178599
Yash Bajoria <yashbajoria05@gmail.com>
2024-09-24 19:27:06 +0530

    first commit

--- registration_form/src/Validation.js
+++ registration_form/src/Validation.js
@@ -1,44 +1,28 @@
-export default function SignupValidation(values) {
+export default function Signupvaluesalidation(values) {
   let error = {};
-  const email_pattern = "/[a-zA-Z0-9]+\\. [a-zA-Z0-9]+@gmail\\.com/";
-  const password_pattern =
-    "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[!@#$%^&*?])[A-Za-zd!@#$%^&*?]{6,}$";
+  const email_pattern = /^[a-z0-9][w.]+@w+?(.w+){1,}$/gi;
 
-  if (values.first_name === "")
-    error.first_name = "First name shouldn't be empty";
-  else error.first_name = "";
-
-  if (values.last_name === "") error.last_name = "Last name shouldn't be empty";
-  else error.last_name = "";
-
-  if (values.email === "") error.email = "Email shouldn't be empty";
-  //   else if (!email_pattern.test(values.email))
-  //     error.email = "Email is Incorrect";
-  else error.email = "";
-
-  if (values.password === "") error.password = "Password shouldn't be empty";
-  //   else if (!password_pattern.test(values.password))
-  //     error.password = "Password is Incorrect";
-  else error.password = "";
-
+  if (!values.first_name) error.first_name = "First name shouldn't be empty";
+  if (!values.last_name) error.last_name = "Last name shouldn't be empty";
+  if (!values.email) error.email = "Email shouldn't be empty";
+  else if (!email_pattern.test(values.email))
+    error.email = "Email is Incorrect";
+  if (!values.password) error.password = "Password shouldn't be empty";
+  else if (values.password.length < 6)
+    error.password = "Atleast 6 characters required";
   return error;
 }
 
-export function LoginValidation(values) {
+export function Loginvaluesalidation(values) {
   let error = {};
-  const email_pattern = "^w+@[a-zA-Z_]+?.[a-zA-Z]{2,3}$";
-  const password_pattern =
-    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@[-`{-~]).{6,}$";
+  const email_pattern = /^[a-z0-9][w.]+@w+?(.w+){1,}$/gi;
 
-  if (values.email === "") error.email = "Email shouldn't be empty";
-  // else if (!email_pattern.test(values.email))
-  //   error.email = "Email is Incorrect";
-  else error.email = "";
-
-  if (values.password === "") error.password = "Password shouldn't be empty";
-  // else if (!password_pattern.test(values.password))
-  //   error.password = "Password is Incorrect";
-  else error.password = "";
+  if (!values.email) error.email = "Email shouldn't be empty";
+  else if (!email_pattern.test(values.email))
+    error.email = "Email is Incorrect";
+  if (!values.password) error.password = "Password shouldn't be empty";
+  else if (values.password.length < 6)
+    error.password = "Atleast 6 characters required";
 
   return error;
 }
